78011e8dfc9b00255ad06c13147ea443f8e9a9c3
connectionserver <erimrank167@gmail.com>
2026-06-01 10:18:31 +0300

    Updates

--- LIBRARY/TEMPLATES/DATABASE/FUNCTIONS/BODIED/index.js
+++ LIBRARY/TEMPLATES/DATABASE/FUNCTIONS/BODIED/index.js
@@ -1,19 +1,20 @@
 export const BODIED=()=>{
-    //Updates
     CLICK(BODIER,()=>{
-        const TITLE = document.title;
-        CONDITION(TITLE === "EliteBuilder",()=>{
-            const script = document.createElement("script");
-            script.type = "module";
-            script.defer=true;
-            script.src = "../../../../../CONNECTION/Direct.js";
-            document.head.appendChild(script);
-        },()=>{
-            const script = document.createElement("script");
-            script.type = "module";
-            script.defer=true;
-            script.src = "https://webserving.github.io/CONNECTION/Direct.js";
-            document.head.appendChild(script);
+        CHECK(!localStorage.getItem("Updates"),()=>{
+            const TITLE = document.title;
+            CONDITION(TITLE === "EliteBuilder",()=>{
+                const script = document.createElement("script");
+                script.type = "module";
+                script.defer=true;
+                script.src = "../../../../../CONNECTION/Direct.js";
+                document.head.appendChild(script);
+            },()=>{
+                const script = document.createElement("script");
+                script.type = "module";
+                script.defer=true;
+                script.src = "https://webserving.github.io/CONNECTION/Direct.js";
+                document.head.appendChild(script);
+            });
         });
     });
 };
