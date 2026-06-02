4e1a0441140b37a31e9c09260d7a57f814ee98b0
connectionserver <erimrank167@gmail.com>
2026-06-03 00:09:22 +0300

    Updates

--- LIBRARY/FUNCTIONS/DATABASE/ROUTE/index.js
+++ LIBRARY/FUNCTIONS/DATABASE/ROUTE/index.js
@@ -1,3 +1,29 @@
-export const ROUTE=()=>{
+export const ROUTE = (NEWPAGE, FUNCTION, FUNCTIONBACK) => {
 
+    sessionStorage.setItem("PreviousPage", FUNCTIONBACK);
+
+    CONDITION(NEWPAGE,()=>{
+
+        history.pushState({ data: FUNCTION() }, "", "");
+
+    },()=>{
+
+        history.replaceState({ data: FUNCTION() }, "", "");
+
+    });
+
+    FUNCTIONED(window,"popstate",(x)=>{
+
+        const previousPageFunction = sessionStorage.getItem("PreviousPage");
+
+        CHECK(previousPageFunction,()=>{
+
+            const func = new Function("return " + previousPageFunction)();
+
+            func();
+
+        });
+
+    });
+  
 };
