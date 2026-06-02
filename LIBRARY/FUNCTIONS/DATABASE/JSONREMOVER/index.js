de091d30742fe0be1f7ce211f8c1fc52adddbff4
connectionserver <erimrank167@gmail.com>
2026-06-03 00:02:22 +0300

    Updates

--- LIBRARY/FUNCTIONS/DATABASE/JSONREMOVER/index.js
+++ LIBRARY/FUNCTIONS/DATABASE/JSONREMOVER/index.js
@@ -1,3 +1,11 @@
-export const JSONREMOVER=()=>{
-
+export const JSONREMOVER = (DATA,ITEM,CALLBACK) => {
+    try {
+        const CurrentData =Array.isArray(DATA)?DATA:[DATA];
+        const Result =CurrentData.filter(data => {
+            return JSON.stringify(data) !==JSON.stringify(ITEM);
+        });
+        CALLBACK(Result);
+    } catch {
+        CALLBACK([]);
+    };
 };
