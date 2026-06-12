c2e2ba3960e1614020f64f645638dd80846e2c3a
eliteserving <erimrank167@gmail.com>
2026-06-12 10:26:41 +0300

    Updates

--- LIBRARY/SERVER/DATABASE/CREATEDATABASE/index.js
+++ LIBRARY/SERVER/DATABASE/CREATEDATABASE/index.js
@@ -14,7 +14,7 @@ export const CREATEDATABASE=(NAME,CALLBACK)=>{
                         });
                     });
                 });
-            })
+            });
         });
     });
 }; 
