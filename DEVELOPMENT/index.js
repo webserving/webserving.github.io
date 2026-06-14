f339f7480f60d26b82005758169551773cc9e7e3
eliteserving <erimrank167@gmail.com>
2026-06-14 18:40:20 +0300

    Updates

--- DEVELOPMENT/index.js
+++ DEVELOPMENT/index.js
@@ -2,12 +2,6 @@ const LINK="https://eliteserving.github.io/LIBRARY/DEMO/Data.json";
 
 const MOBILEVIEW=()=>{
 
-    GETDATA(MAINCONNECTIONAPI,"DATABASES",(DARA)=>{
-        
-        console.log(DARA)
-
-    });
-
     CLEAR("");
 
     VIEWS("",(HOLDER)=>{
@@ -20,11 +14,7 @@ const MOBILEVIEW=()=>{
     
             });
     
-            HEADER(DATA,"NCOO",()=>{
-    
-            });
-    
-            ICONS(DATA,BLACKUSERICON,WHITEUSERICON,()=>{
+            HEADER(DATA,"ShylanSiraj Fashion Hub",()=>{
     
             });
     
@@ -34,191 +24,6 @@ const MOBILEVIEW=()=>{
     
         });
     
-        INLINEVIEW(HOLDER,(DATA)=>{
-    
-            HEIGHT(DATA,"auto");
-    
-            FETCH(LINK,"",(Data)=>{
-    
-                CLEAR(DATA);
-    
-                REDUX(Data,(Info)=>{
-    
-                    VIEWS(DATA,(DATATED)=>{
-    
-                        OVERFLOW(DATATED,"hidden");
-                        FLEXSHRINK(DATATED);
-                        MARGIN(DATATED,"5%");
-                        WIDTH(DATATED,"80px");
-                        HEIGHT(DATATED,"80px");
-                        BORDERRADIUS(DATATED,"50px");
-                        BACKGROUND(DATATED,"blue");
-                        BORDER(DATATED,"white");
-    
-                    });
-    
-                });
-    
-            });
-    
-        });
-    
-        VIEWS(HOLDER,(DATA)=>{
-    
-            WIDTH(DATA,"98%");
-            BORDER(DATA,"#FFFFFF10");
-            HEIGHT(DATA,"auto");
-    
-            HEADER(DATA,"Quick Donation",(DATATED)=>{
-    
-                MARGIN(DATATED,"5%");
-    
-            });
-    
-            INLINEVIEW(DATA,(DATATED)=>{
-    
-                HEIGHT(DATATED,'80px');
-    
-                BUTTON(DATATED,"Zakat",(DATATID)=>{
-    
-                    MARGIN(DATATID,"");
-                    WIDTH(DATATID,"35%");
-    
-                });
-    
-                INPUT(DATATED,"tel","Enter Amount",(DATATID)=>{
-    
-                    MARGIN(DATATID,"");
-                    HEIGHT(DATATID,"50px");
-                    WIDTH(DATATID,"53%");
-    
-                });
-    
-            });
-    
-            BUTTON(DATA,"Donate Now",(DATATID)=>{
-    
-                MARGIN(DATATID,"2%");
-    
-                BACKGROUND(DATATID,"forestgreen");
-    
-                COLOR(DATATID,"WHITE");
-    
-            });
-    
-        });
-    
-        INLINEVIEW(HOLDER,(DATA)=>{
-    
-            HEIGHT(DATA,"70%");
-
-            VIEWS(DATA,(DATATED)=>{
-    
-                OVERFLOW(DATATED,"hidden");
-                FLEXSHRINK(DATATED);
-                MARGIN(DATATED,"2%");
-                WIDTH(DATATED,"95%");
-                HEIGHT(DATATED,"95%");
-                BORDERRADIUS(DATATED,"5px");
-                BACKGROUND(DATATED,"blue");
-                BORDER(DATATED,"#FFFFFF10");
-    
-            });
-
-            VIEWS(DATA,(DATATED)=>{
-    
-                OVERFLOW(DATATED,"hidden");
-                FLEXSHRINK(DATATED);
-                MARGIN(DATATED,"2%");
-                WIDTH(DATATED,"95%");
-                HEIGHT(DATATED,"95%");
-                BORDERRADIUS(DATATED,"5px");
-                BACKGROUND(DATATED,"blue");
-                BORDER(DATATED,"#FFFFFF10");
-    
-            });
-
-            VIEWS(DATA,(DATATED)=>{
-    
-                OVERFLOW(DATATED,"hidden");
-                FLEXSHRINK(DATATED);
-                MARGIN(DATATED,"2%");
-                WIDTH(DATATED,"95%");
-                HEIGHT(DATATED,"95%");
-                BORDERRADIUS(DATATED,"5px");
-                BACKGROUND(DATATED,"blue");
-                BORDER(DATATED,"#FFFFFF10");
-    
-            });
-    
-        });
-
-        HEADER(HOLDER,"Appeals That need your support",(DATATED)=>{
-        
-            MARGIN(DATATED,"5%");
-        
-        });
-
-        VIEWS(HOLDER,(DATA)=>{
-            
-            WIDTH(DATA,"100%");
-            HEIGHT(DATA,"");
-            BACKGROUND(DATA,"transparent");
-            MARGINRIGHT(DATA,"2%")
-        
-            FETCH(LINK,"",(Data)=>{
-    
-                REDUX(Data,()=>{
-
-                    BREAK(DATA,()=>{
-            
-                    });
-    
-                    VIEWS(DATA,(DATATA)=>{
-                
-                        WIDTH(DATATA,"90%");
-                        HEIGHT(DATATA,"70%");
-                        BORDERRADIUS(DATATA,"10px");
-                        BACKGROUND(DATATA,"orange");
-                        MARGINRIGHT(DATATA,"2%")
-            
-                    });
-            
-                });
-    
-            });
-        });
-
-        BREAK(HOLDER,()=>{
-            
-        });
-
-        VIEWS(HOLDER,(DATA)=>{
-            
-            WIDTH(DATA,"100%");
-            HEIGHT(DATA,"50%");
-            BACKGROUND(DATA,"red");
-            MARGINRIGHT(DATA,"2%")
-
-        });
-
-        BREAK(HOLDER,()=>{
-            
-        });
-
-        VIEWS(HOLDER,(DATA)=>{
-            
-            WIDTH(DATA,"100%");
-            HEIGHT(DATA,"80%");
-            BACKGROUND(DATA,"green");
-            MARGINRIGHT(DATA,"2%")
-
-        });
-
-        BREAK(HOLDER,()=>{
-            
-        });
-
     });
   
 };
